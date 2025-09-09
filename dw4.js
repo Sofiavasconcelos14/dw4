2fe4f41bd6e0b4f3c42051790bfe19d0e1c12317
Sofiavasconcelos14 <Sofiaconceicao363@gmail.com>
2025-09-08 22:20:56 -0400

    novo

---
+++ dw4.js
@@ -0,0 +1,57 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirmation = document.getElementById('password-confirmation')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkInputs()
+})
+
+function checkInputs(){
+    const usernameValue = username.value
+    const emailValue = email.value
+    const passwordValue =  password.value
+    const  passwordConfirmationValue =  passwordConfirmation.value
+
+
+ 
+if(usernameValue === ''){
+    setErroFor(username, 'O nome do usuario é obrigatorio')
+}  else {
+    setSuccessFor(username)
+}
+if(emailValue === ''){
+    setErroFor(email, 'O nome do usuario é obrigatorio')
+}  else {
+    setSuccessFor(email)
+    }
+    
+}
+
+    
+
+function setErroFor(input, message){
+   const formControl = input.parentElement;
+   const small = formControl.querySelector('small')
+
+   small.innertext = message 
+
+   formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+   const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+} 
+
+
+
+
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+    );
+}
